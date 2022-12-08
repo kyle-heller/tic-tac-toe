a21f25ae5da6d2b290e9073f8f1bbfa9d9425ecb
kyle.heller <kyle.heller@gmail.com>
2022-12-07 19:51:06 -0500

    Hardest part done, ASCII Grid lol

---
+++ tic_tac_toe.rb
@@ -0,0 +1,29 @@
+class X
+  
+end
+
+class O
+  
+end
+
+class Square
+  
+end
+
+class Grid
+  
+end
+    
+ 
+puts "
+    ╭    ╮╭    ╮╭    ╮
+    | 1  || 2  || 3  |
+    ╰    ╯╰    ╯╰    ╯
+    ╭    ╮╭    ╮╭    ╮
+    | 4  || 5  || 6  |
+    ╰    ╯╰    ╯╰    ╯
+    ╭    ╮╭    ╮╭    ╮
+    | 7  || 8  || 9  |
+    ╰    ╯╰    ╯╰    ╯
+
+    "
